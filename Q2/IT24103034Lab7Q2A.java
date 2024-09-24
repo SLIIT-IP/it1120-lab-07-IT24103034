304238103bae0d7b6413bc1621ff8438462fdc33
IT24103034 <it24103034@my.sliit.lk>
2024-09-24 20:52:46 +0530

    Add files via upload

---
+++ Q2/IT24103034Lab7Q2A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IT24103034Lab7Q2A {
+
+	public static void main(String[] args) {
+
+		int i,j;
+
+		Scanner input=new Scanner(System.in);
+ 
+		for(i=1;i<=3;i++)
+		{
+			for(j=1;j<=4;j++)
+			{
+				System.out.print("$"+"");
+			}
+			System.out.print("\n ");
+		}
+	}
+}
