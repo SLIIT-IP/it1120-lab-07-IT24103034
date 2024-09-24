2474b9cca93bbf8f051da22dcae67367600e9f03
IT24103034 <it24103034@my.sliit.lk>
2024-09-24 20:54:18 +0530

    Add files via upload

---
+++ Q2/IT24103034Lab7Q2C.java
@@ -0,0 +1,20 @@
+public class IT24103034Lab7Q2C {
+
+	public static void main(String[] args) {
+
+		int i,j,k=5;
+
+		for(i=5;i>=1;i--)
+		{
+			System.out.print(i);
+
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(i);
+			}
+
+			System.out.println();
+						
+		}
+	}
+}
